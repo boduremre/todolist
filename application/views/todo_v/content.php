57e789bea1eee04b3d5b0c07831efc9a56b9dc13
Emre Bodur <emrebodurbto@gmail.com>
2020-06-11 01:56:48 +0300

    Todo formu csrf uyumlu hale getirildi.

--- application/views/todo_v/content.php
+++ application/views/todo_v/content.php
@@ -1,17 +1,13 @@
 <div class="row">
     <div class="col-md-12 col-sm-12">
-        <form action="<?php echo base_url('/index.php/todo/insert'); ?>" method="post">
+        <?php echo form_open(base_url('/index.php/todo/insert')); ?>
             <div class="row">
                 <div class="form-group col-md-12 col-sm-12">
                     <input type="text" name="todo_title" class="form-control"
                            placeholder="Yapılacak işi yaz entere bas..." required autocomplete="off">
                 </div>
-
-                <?php /*<div class="col-md-1 col-sm-1">
-                        <button type="submit" class="btn btn-primary ">Kaydet</button>
-                    </div> */ ?>
             </div>
-        </form>
+        <?php echo form_close(); ?>
     </div>
 </div>
 <div class="row">
@@ -26,13 +22,6 @@
                     <!--<th scope="col">İşlemler</th>-->
                 </tr>
                 </thead>
-                <!--                <tfoot>-->
-                <!--                <tr>-->
-                <!--                    <th colspan="3" style="text-align: right;font-size: 11px;font-weight: normal">Sayfa <strong>{elapsed_time}</strong>-->
-                <!--                        saniyede oluşturuldu.-->
-                <!--                    </th>-->
-                <!--                </tr>-->
-                <!--                </tfoot>-->
                 <tbody>
                 <?php foreach ($items as $item) { ?>
                     <tr data-gorev-id="<?php echo $item->id; ?>">
@@ -40,11 +29,11 @@
                         <td>
                             <?php echo ' <span class="badge badge-primary"><small>' . date("d-m-Y H:i:s", strtotime($item->createdDate)) . '</small></span>'; ?>
                             <?php
-                                if ($item->completedDate != "0000-00-00 00:00:00") {
-                                    echo ' <span class="badge badge-success"><small>' . date("d-m-Y H:i:s", strtotime($item->completedDate)) . '</small></span>';
-                                } else {
-                                    echo ' <span class="badge badge-danger"><small>TAMAMLANMADI</small></span>';
-                                }
+                            if ($item->completedDate != "0000-00-00 00:00:00") {
+                                echo ' <span class="badge badge-success"><small>' . date("d-m-Y H:i:s", strtotime($item->completedDate)) . '</small></span>';
+                            } else {
+                                echo ' <span class="badge badge-danger"><small>TAMAMLANMADI</small></span>';
+                            }
                             ?>
                         </td>
                         <td style="width: 10%">
@@ -56,17 +45,6 @@
                                        type="checkbox" class="js-switch" disabled/>
                             <?php } ?>
                         </td>
-                        <!--<td style="width: 5%">
-                        <a href="<?php // echo base_url("/index.php/todo/delete/$item->id"); ?>"
-                           class="btn btn-sm btn-danger"
-                           data-toggle="confirmation"
-                           data-singleton="true"
-                           data-title="Emin misiniz?"
-                           data-btn-cancel-label="Hayır"
-                           data-btn-cancel-class="btn btn-sm btn-success"
-                           data-btn-ok-label="Evet"
-                           data-btn-ok-class="btn btn-sm btn-danger">Sil</a>
-                    </td> -->
                     </tr>
                 <?php } ?>
                 </tbody>
